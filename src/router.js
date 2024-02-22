572567235f67c55695a2a09f9d8bf2453f5ac8f2
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-02-22 10:50:35 +0100

    Add: router; home page; axios fetch to restaurant

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory} from "vue-router";
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+    {
+       
+    },
+    {
+       
+       
+    },
+    {
+
+    }, 
+    {
+        
+    }
+    ],
+    
+});
+
+export { router };

413b2ef9529883208bc979543ac4527287cefbfc
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-02-23 18:06:38 +0100

    WIP: filters for categories

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -1,10 +1,61 @@
 <script>
 
+import axios from 'axios';
+import { store } from '../store';
+import { router } from '../router';
+
 
 export default {
     data() {
-        return {}
+        return {
+            store,
+            cusine_types: [],
+            selectedCuisine: '',
+            restaurants: []
+        }
     },
+    created(){
+        this.fetchCuisines();
+
+    },
+    methods: {
+        fetchCuisines() {
+            axios.get(`${this.store.baseUrl}/api/cusine_types`, {
+
+            })
+                .then((resp) => {  
+                    console.log(resp);
+                    this.cusine_types = resp.data.results; 
+                    console.log(resp.data.results);
+
+                })
+                .catch((error) => {
+                    this.error = error;
+
+                });
+        },
+        fetchRestaurants(){
+            console.log(this.selectedCuisine);
+            if (!this.selectedCuisine) return;
+            axios.get(`${this.store.baseUrl}/api/restaurants/cusine_type/${this.selectedCuisine}`, {
+                params: {
+                    page: this.currentPage, // includi il numero pag corrente come parametro query x paginazione
+                }
+            })
+                .then((resp) => {  //se risposta positiva
+                    console.log(resp);
+                    this.restaurants = resp.data.results.data;  
+                    console.log(resp.data.results);
+                    this.lastPage = resp.data.results.lastPage; // aggiorna lastpage da risposta
+                    this.total = resp.data.results.total; // aggiorna total da risposta
+                    this.isLoading = false;
+                })
+                .catch((error) => { // risposta negativa 
+                    this.error = error; // salva errori nei dati del componente 
+                    this.isLoading = false;
+                });
+        }
+    }
 }
 
 </script>
@@ -26,12 +77,13 @@ export default {
                 <!-- SELECT FILTER -->
                 <div class="input-group mb-3 mt-5 d-flex align-items-center justify-content-center">
                     <div class="input-group-text p-0">
-                        <select class="form-select form-select-lg shadow-none bg-light border-0">
+                        <select v-model="selectedCuisine" class="form-select form-select-lg shadow-none bg-light border-0">
                             <option>Categories</option>
+                            <option v-for="cusine_type in cusine_types" :value="cusine_type.name">{{ cusine_type.name }}</option>
                         </select>
                     </div>
                     <!-- SEARCH BUTTON -->
-                    <button class="btn btn-warning">
+                    <button class="btn btn-warning" @click="fetchRestaurants">
                         Search
                     </button>
                 </div>

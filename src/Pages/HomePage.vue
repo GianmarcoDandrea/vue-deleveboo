b87ca673e88da3cd7e21a2802b78dd45a7177b46
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2024-02-26 17:48:56 +0100

    fix RestaurantsList

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -3,6 +3,8 @@
 import axios from 'axios';
 import { store } from '../store';
 import { router } from '../router';
+import RestaurantsList from '../components/RestaurantsList.vue';
+
 
 
 export default {
@@ -10,31 +12,34 @@ export default {
         return {
             store,
             cusine_types: [],
-            selectedCuisine: '',
+            selectedCusines: [],
             restaurants: [],
             filteredRestaurants: [],
+            showRestaurants: false,
         }
     },
+    components: { RestaurantsList },
     created() {
-        this.fetchCuisines();
+        this.fetchCusines();
         this.fetchAllRestaurants();
 
     },
     watch: {
-        selectedCuisine(newVal, oldVal) {
-            this.filterRestaurantsByCuisine();
+        selectedCusines(newVal, oldVal) {
+            //this.filterRestaurantsByCusine();
             console.log(newVal, oldVal);
         }
     },
     methods: {
+
         fetchAllRestaurants() {
 
             axios.get(`${this.store.baseUrl}/api/restaurants`, {
             })
                 .then((resp) => {  //se risposta positiva
-                    console.log(resp);
+                    //console.log(resp);
                     this.restaurants = resp.data.results.data;  // aggiorna progetti con dati da risposta
-                    console.log(resp.data.results);
+                    console.log(this.restaurants);
 
 
                 })
@@ -43,14 +48,14 @@ export default {
 
                 });
         },
-        fetchCuisines() {
+        fetchCusines() {
             axios.get(`${this.store.baseUrl}/api/cusine_types`, {
 
             })
                 .then((resp) => {
-                    console.log(resp);
+                    //console.log(resp);
                     this.cusine_types = resp.data.results;
-                    console.log(resp.data.results);
+                    console.log(this.cusine_types);
 
                 })
                 .catch((error) => {
@@ -58,39 +63,27 @@ export default {
 
                 });
         },
-        // fetchRestaurants(){
-        //     console.log(this.selectedCuisine);
-        //     if (!this.selectedCuisine) return;
-        //     axios.get(`${this.store.baseUrl}/api/restaurants/cusine_type/${this.selectedCuisine}`, {
-        //         params: {
-        //             page: this.currentPage, // includi il numero pag corrente come parametro query x paginazione
-        //         }
-        //     })
-        //         .then((resp) => {  //se risposta positiva
-        //             console.log(resp);
-        //             this.restaurants = resp.data.results.data;  
-        //             console.log(resp.data.results);
-        //             this.lastPage = resp.data.results.lastPage; // aggiorna lastpage da risposta
-        //             this.total = resp.data.results.total; // aggiorna total da risposta
-        //             this.isLoading = false;
-        //         })
-        //         .catch((error) => { // risposta negativa 
-        //             this.error = error; // salva errori nei dati del componente 
-        //             this.isLoading = false;
-        //         });
-        // },
-        filterRestaurantsByCuisine() {
-            //controlla se selctedcuisine è messo oppure no, se non è messo non viene effettuato filtro 
-            if (!this.selectedCuisine || this.selectedCuisine === "Categories") {
-                return;
+
+        filterRestaurantsByCusine() {
+            ///se nessun filtro è impostato, ritorna tutti i ristoranti
+            if (this.selectedCusines.length === 0) {
+                this.filteredRestaurants = this.restaurants;
+            } else {
+                //se ci sono filtri impostati, filtra i ristoranti
+                this.filteredRestaurants = this.restaurants.filter(restaurant =>
+                    this.selectedCusines.every(selectedCusines => restaurant.cusine_types.some(cusine => cusine.name === selectedCusines))
+                );
             }
-            //filtrare array ristoranti per inclusdere solo quei ristoranti che hanno cuisine type == selected cuisine.
-            this.filteredRestaurants = this.restaurants.filter(restaurant =>
-                //some: testa se almeno un elemento di cuisine type array è uguale al testo di ricerca
-                restaurant.cusine_types.some(cuisine => cuisine.name === this.selectedCuisine)
-            );
-            console.log(this.filteredRestaurants, this.selectedCuisine);
-        }
+            this.showRestaurants = true
+            console.log(this.filteredRestaurants, this.selectedCusines);
+            //se nessun ristorante, messaggio
+            if (this.filteredRestaurants.length === 0) {
+                this.noRestaurantsMessage = "nessun ristorante trovato"
+                console.log(this.noRestaurantsMessage)
+            } else {
+                this.noRestaurantsMessage = "";
+            }
+        },
     }
 }
 
@@ -123,11 +116,15 @@ export default {
                                             </button>
                                             <ul class="dropdown-menu dropdown-menu-lg-end list">
                                                 <li class="list-group-category">
-                                                    <div class="d-flex align-items-center check text-white justify-content-center my_checkbox">
+                                                    <div
+                                                        class="d-flex align-items-center check text-white justify-content-center my_checkbox">
                                                         <div class="p-3 " v-for="cusine_type in cusine_types"
                                                             :value="cusine_type.name">
-                                                            <input class="" :id="'cusine_type-' + cusine_type.id" type="checkbox" v-model="selectedCuisines" :value="cusine_type.name">
-                                                            <label class="ms-2 " :for="'cusine_type-' + cusine_type.id">{{ cusine_type.name }}</label>
+                                                            <input class="" :id="'cusine_type-' + cusine_type.id"
+                                                                type="checkbox" v-model="selectedCusines"
+                                                                :value="cusine_type.name">
+                                                            <label class="ms-2 " :for="'cusine_type-' + cusine_type.id">{{
+                                                                cusine_type.name }}</label>
                                                         </div>
                                                     </div>
                                                 </li>
@@ -139,7 +136,7 @@ export default {
                             </div>
 
                             <!-- SEARCH BUTTON -->
-                            <button class="btn btn-warning" @click="filterRestaurantsByCuisine">
+                            <button class="btn btn-warning" @click="filterRestaurantsByCusine">
                                 Search
                             </button>
                         </div>
@@ -148,6 +145,9 @@ export default {
 
             </div>
         </div>
+    </div>
+    <div v-if="showRestaurants">
+            <RestaurantsList :restaurants="filteredRestaurants" />
     </div>
 </template>
 
@@ -180,6 +180,7 @@ export default {
         display: block;
     }
 }
+
 // MEDIA QUERY
 
 .my_checkbox {
@@ -283,5 +284,4 @@ p {
 
 button {
     height: 50px;
-}
-</style>
+}</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory} from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import RestaurantDetailsPage from "./pages/RestaurantDetailsPage.vue";
-import RestaurantsList from "./pages/RestaurantsList.vue";
+//import RestaurantsList from "./pages/RestaurantsList.vue";
 import NotFoundPage from "./pages/NotFoundPage.vue"
 
 
@@ -11,21 +11,21 @@ const router = createRouter({
     {
         path: "/",
         name: "home",
-        component:HomePage,
+        component: HomePage,
        
     },
     {
         path: "/restaurants/:slug",
         name: "restaurant-details-page",
-        component:RestaurantDetailsPage,
+        component: RestaurantDetailsPage,
        
     },
+    // {
+    //     path:"/restaurants",
+    //     name:"restaurantsList",
+    //     component: RestaurantsList,
+    // },
     {
-        path: "/restaurants",
-        name: "restaurants-list",
-        component: RestaurantsList,
-    },
-     {
         path: '/:pathMatch(.*)*',
         name: 'not-found',
         component: NotFoundPage,

1e12e105bc76066fd77b096cfd7725ed39618130
GianmarcoDandrea <gianm96@hotmail.it>
2024-02-28 11:26:17 +0100

    add: responsive settings(mobile first) in header

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory} from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import RestaurantDetailsPage from "./pages/RestaurantDetailsPage.vue";
-import CheckoutPage from "./components/CheckoutPage.vue";
-//import RestaurantsList from "./pages/RestaurantsList.vue";
 import NotFoundPage from "./pages/NotFoundPage.vue"
 import PaymentPage from "./pages/PaymentPage.vue"
 
